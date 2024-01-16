4866f77fc631234575bc851e15433d9a9d21d465
Sahil H. Moubaideen <sahil.mubaideen@network.global>
2024-01-16 13:29:57 +0300

    scheduling sending reminders

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send event reminders to attendees';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = \App\Models\Event::with('attendees.user')->whereBetween('start_time', [now(), now()->addDay()])->get();
+        foreach ($events as $event) {
+            foreach ($event->attendees as $attendee) {
+                $this->info('' . $attendee->id);
+                // $attendee->user->notify(new \App\Notifications\EventReminder($event));
+            }
+        }
+    }
+}
